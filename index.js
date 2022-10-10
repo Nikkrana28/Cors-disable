2f1b85ead02570ce65c9426f2f21d0d60283cb8d
Nikhil Kharana <60419615+Nikkrana28@users.noreply.github.com>
2022-10-10 17:37:44 +0530

    change id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const { set } = require('mongoose')
 const url = 'https://close.ap.ngrok.io'
-const id = '6344075c8cf739a0d458c1e2'
+const id = '63440ad095dfafb67227c9cd'
 
 // function run() {
 //     setInterval(async()=>{
